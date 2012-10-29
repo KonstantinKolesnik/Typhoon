f636215fbd2a9a6d22c5cedde11a3a4b6769bc9e
Konstantin Kolesnik <kk@techwire.dp.ua>
2012-10-29 14:31:10 +0200

    Typhoon.WPClient

---
+++ Typhoon.WPClient/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace Typhoon.WPClient
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (!App.ViewModel.IsDataLoaded)
+            //    App.ViewModel.LoadData();
+        }
+    }
+}
---
+++ Typhoon.WPClient/ViewModels/ItemViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Typhoon.WPClient
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private string _lineOne;
+        private string _lineTwo;
+        private string _lineThree;
+        
+        public string LineOne
+        {
+            get
+            {
+                return _lineOne;
+            }
+            set
+            {
+                if (value != _lineOne)
+                {
+                    _lineOne = value;
+                    NotifyPropertyChanged("LineOne");
+                }
+            }
+        }
+        public string LineTwo
+        {
+            get
+            {
+                return _lineTwo;
+            }
+            set
+            {
+                if (value != _lineTwo)
+                {
+                    _lineTwo = value;
+                    NotifyPropertyChanged("LineTwo");
+                }
+            }
+        }
+        public string LineThree
+        {
+            get
+            {
+                return _lineThree;
+            }
+            set
+            {
+                if (value != _lineThree)
+                {
+                    _lineThree = value;
+                    NotifyPropertyChanged("LineThree");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
